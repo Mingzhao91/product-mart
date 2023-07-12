cf71361bb44ac132f63016aec7201fcb2f69bc3d
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-12 21:06:32 +0100

    get data from local file and show them in html

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ProductDataService } from '../product-data.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
+  products!: Observable<any>;
+  constructor(private productDataService: ProductDataService) {}
 
+  ngOnInit() {
+    this.products = this.productDataService.getAllProducts();
+  }
 }
