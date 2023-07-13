b5921992636f8458a1b6764f2eba081861aa7fcd
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-13 21:05:12 +0100

    create header toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatCardModule, MatButtonModule],
+  exports: [MatCardModule, MatButtonModule, MatToolbarModule],
 })
 export class MaterialModule {}
