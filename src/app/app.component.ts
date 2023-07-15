847bf41424ba65334ae686267cd9663343455b90
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-15 17:15:40 +0100

    store logged in user and logout user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { AuthService } from './auth.service';
+import { User } from './user.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'product-mart';
+  user!: User;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.authService.user
+      .pipe(takeUntilDestroyed())
+      .subscribe((user) => (this.user = user));
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
+  }
 }
