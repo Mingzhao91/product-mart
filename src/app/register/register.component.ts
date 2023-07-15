655846d21a935b2d0f08de81a6deef409eae7485
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-15 17:14:54 +0100

    code refactoring

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,32 +1,33 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService, User } from '../auth.service';
+import { User } from '../user.interface';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
   userForm = this.fb.group({
     fullname: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]]
+    password: ['', [Validators.required]],
   });
 
-  constructor(private fb: FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService) {}
+    private authService: AuthService
+  ) {}
 
   register() {
     const user = this.userForm.getRawValue();
 
-    this.authService.register(user as User)
-    .subscribe(()=> {
+    this.authService.register(user as User).subscribe(() => {
       this.router.navigateByUrl('/');
     });
   }
-
 }
