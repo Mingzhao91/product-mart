96f1d02f29b39750f1610c336e047f3b7d32d4c1
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-15 17:11:26 +0100

    add material modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,9 +4,19 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  exports: [MatCardModule, MatButtonModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
 })
 export class MaterialModule {}
