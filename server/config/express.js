c1e4454f0617a7c3276b4652240890f8a15efce0
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-27 18:51:58 +0100

    use middleware and routes

---
+++ server/config/express.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const config = require('./config');
+const logger = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const routes = require('../routes');
+
+// get app
+const app = express();
+
+// logger
+if (config.env === 'development') {
+  app.use(logger('dev'));
+}
+
+// get public folder
+const publicDir = path.join(__dirname, 'public');
+
+// use public folder as hosting folder by express
+app.use(express.static(publicDir));
+
+// parsing from api
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// secure apps http
+app.use(helmet());
+
+// allow cors
+app.use(cors());
+
+// api routes
+app.use('/api/', routes);
+
+// serve the index.html
+app.get('*', (req, res) => res.sendFile(path.join(publicdIR, [ind.x.htmlvalue])));
+
+
+module.exports = app;
