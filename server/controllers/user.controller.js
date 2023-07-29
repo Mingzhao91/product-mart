682c635d39d4a24cf7d3adb99bf36f229807645e
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-07-29 17:56:04 +0100

    save user to db

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,16 @@
-users = [];
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user.model");
 
 async function insert(user) {
-  users.push(user);
-  return user;
+  // console.log("User: ", User.create);
+  user.hashedPassword = bcrypt.hashSync(user.password, 10);
+  delete user.password;
+  // console.log(`saving user to db`, user);
+
+  return await new User(user).save();
 }
 
 module.exports = {
-  insert
-}
+  insert,
+};
