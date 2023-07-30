997f6f3f40664b20f7e0126d47586a6b6f4d8e4f
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-07-30 19:59:25 +0100

    send token and user back

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,34 +1,37 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const asyncHandler = require("express-async-handler");
 
 const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-router.post("/register", asyncHandler(insert));
-router.post("/login", asyncHandler(getUserByEmailIdAndPassword));
+router.post("/register", asyncHandler(insert), login);
+router.post("/login", asyncHandler(getUserByEmailIdAndPassword), login);
 
 async function insert(req, res, next) {
   const user = req.body;
   console.log("registering user");
-  const savedUser = await userController.insert(user);
-  console.log("done");
-  return res.json(savedUser);
+  req.user = await userController.insert(user);
+  next();
 }
 
 async function getUserByEmailIdAndPassword(req, res, next) {
   const user = req.body;
   console.log(`searching user for `, user);
 
-  const savedUser = await userController.getUserByEmailIdAndPassword(
+  req.user = await userController.getUserByEmailIdAndPassword(
     user.email,
     user.password
   );
 
-  console.log("savedUser: ", savedUser);
+  next();
+}
 
-  return res.json(savedUser);
+async function login(req, res) {
+  const user = req.user;
+  const token = authController.generateToken(user);
+  return res.json({ user, token });
 }
 
 module.exports = router;
