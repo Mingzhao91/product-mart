46d959c6e078d5937d8e439e99041125b5e74e13
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-07-30 19:59:41 +0100

    initialise passport

--- server/config/express.js
+++ server/config/express.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const path = require('path');
-const config = require('./config');
-const logger = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const routes = require('../routes');
+const express = require("express");
+const path = require("path");
+const config = require("./config");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const routes = require("../routes");
+const passport = require("../middleware/passport");
 
 // get app
 const app = express();
 
 // logger
-if (config.env === 'development') {
-  app.use(logger('dev'));
+if (config.env === "development") {
+  app.use(logger("dev"));
 }
 
 // get public folder
-const publicDir = path.join(__dirname, 'public');
+const publicDir = path.join(__dirname, "public");
 
 // use public folder as hosting folder by express
 app.use(express.static(publicDir));
 
 // parsing from api
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // secure apps http
 app.use(helmet());
@@ -32,11 +35,15 @@ app.use(helmet());
 // allow cors
 app.use(cors());
 
+// authenticate
+app.use(passport.initialize());
+
 // api routes
-app.use('/api/', routes);
+app.use("/api/", routes);
 
 // serve the index.html
-app.get('*', (req, res) => res.sendFile(path.join(publicdIR, [ind.x.htmlvalue])));
-
+app.get("*", (req, res) =>
+  res.sendFile(path.join(publicdIR, [ind.x.htmlvalue]))
+);
 
 module.exports = app;
