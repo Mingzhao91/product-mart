bb1ad5323a92596321fbe7d038f0fcd3cf3ae161
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-07-30 10:31:36 +0100

    register routes for login

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,17 +1,34 @@
-const express = require('express');
-const userController = require('../controllers/user.controller');
-const asyncHandler = require('express-async-handler');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const asyncHandler = require("express-async-handler");
+
+const userController = require("../controllers/user.controller");
 
 const router = express.Router();
 
-router.post('/register', asyncHandler(insert));
+router.post("/register", asyncHandler(insert));
+router.post("/login", asyncHandler(getUserByEmailIdAndPassword));
 
 async function insert(req, res, next) {
   const user = req.body;
-  console.log('registering user');
+  console.log("registering user");
   const savedUser = await userController.insert(user);
-  console.log('done');
-  res.json(savedUser);
+  console.log("done");
+  return res.json(savedUser);
+}
+
+async function getUserByEmailIdAndPassword(req, res, next) {
+  const user = req.body;
+  console.log(`searching user for `, user);
+
+  const savedUser = await userController.getUserByEmailIdAndPassword(
+    user.email,
+    user.password
+  );
+
+  console.log("savedUser: ", savedUser);
+
+  return res.json(savedUser);
 }
 
 module.exports = router;
