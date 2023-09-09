4eb8d83f12f87ca4b59d47e7a7a67b6362967966
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-09-09 15:22:11 +0100

    CRUD for cart

--- src/app/core/cart/cart-store.ts
+++ src/app/core/cart/cart-store.ts
@@ -2,10 +2,53 @@ import { Injectable } from '@angular/core';
 import { Store } from '@core/store';
 
 import { CartState, initialState } from './cart-state';
+import { CartItem } from './cart-item';
 
 @Injectable({ providedIn: 'root' })
 export class CartStore extends Store<CartState> {
   constructor() {
     super(initialState);
+  }
+
+  addCartItem(cartItemToAdd: CartItem) {
+    const newState: CartState = {
+      ...this.state,
+      cartItems: [...this.state.cartItems, cartItemToAdd],
+    };
+
+    this.setState(newState);
+  }
+
+  clearCart() {
+    const newState: CartState = initialState;
+    this.setState(newState);
+  }
+
+  restoreCart(stateToRestore: CartState) {
+    this.setState(stateToRestore);
+  }
+
+  removeCartItem(cartItemToRemove: CartItem) {
+    const newState: CartState = {
+      ...this.state,
+      cartItems: this.state.cartItems.filter(
+        (cartItem) => cartItem.productId !== cartItemToRemove.productId
+      ),
+    };
+
+    this.setState(newState);
+  }
+
+  updateCartItem(cartItemToUpdate: CartItem) {
+    const newState: CartState = {
+      ...this.state,
+      cartItems: this.state.cartItems.map((cartItem) =>
+        cartItem.productId === cartItemToUpdate.productId
+          ? cartItemToUpdate
+          : cartItem
+      ),
+    };
+
+    this.setState(newState);
   }
 }
