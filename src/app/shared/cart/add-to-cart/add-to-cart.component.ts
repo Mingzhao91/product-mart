1aa9f5464a9a1bac6270239b12bf44e49c78fa4d
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-09-10 12:42:21 +0100

    add item to cart

--- src/app/shared/cart/add-to-cart/add-to-cart.component.ts
+++ src/app/shared/cart/add-to-cart/add-to-cart.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { Observable } from 'rxjs';
 
 import { CartStore } from '@core/cart/cart-store';
-import { CartService } from '@core/cart/cart.service';
+import {
+  ALLOWED_PRODUCT_QUANTITIES,
+  CartService,
+} from '@core/cart/cart.service';
+import { Product } from '@core/products/product';
+import { getIsItemAlreadyInCart } from '@core/cart/cart-selector';
 
 @Component({
   selector: 'app-add-to-cart',
@@ -9,5 +17,31 @@ import { CartService } from '@core/cart/cart.service';
   styleUrls: ['./add-to-cart.component.scss'],
 })
 export class AddToCartComponent {
-  constructor(private cartStore: CartStore, private cartService: CartService) {}
+  @Input() product!: Product;
+  availableQuantities!: number[];
+  quantity!: number;
+  isItemAlreadyInCart!: Observable<boolean>;
+
+  constructor(
+    private destroyRef: DestroyRef,
+    private cartStore: CartStore,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
+    this.availableQuantities = ALLOWED_PRODUCT_QUANTITIES;
+    this.isItemAlreadyInCart = this.cartStore.select(
+      getIsItemAlreadyInCart(this.product.id)
+    );
+    this.quantity = 1;
+  }
+
+  addItemToCart() {
+    this.cartService
+      .addToCart(this.product, this.quantity)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((cartItem) => {
+        console.log('added to cart', cartItem);
+      });
+  }
 }
