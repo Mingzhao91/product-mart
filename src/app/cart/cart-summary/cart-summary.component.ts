8a989d228ed7a25020ac17b4f8ebbbdeca5e9757
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-09-10 17:52:21 +0100

    show details in cart summary

--- src/app/cart/cart-summary/cart-summary.component.ts
+++ src/app/cart/cart-summary/cart-summary.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CartStore } from '@core/cart/cart-store';
+import {
+  getCartItemsCount,
+  getCartSubTotal,
+  getEstimatedTax,
+  getOrderTotal,
+  getShippingCost,
+} from '@core/cart/cart-selector';
 
 @Component({
   selector: 'app-cart-summary',
   templateUrl: './cart-summary.component.html',
-  styleUrls: ['./cart-summary.component.scss']
+  styleUrls: ['./cart-summary.component.scss'],
 })
 export class CartSummaryComponent {
+  cartSubTotal!: Observable<number>;
+  cartItemsCount!: Observable<number>;
+  shippingCost!: Observable<number>;
+  estimatedTax!: Observable<number>;
+  orderTotal!: Observable<number>;
 
+  constructor(private cartStore: CartStore) {}
+
+  ngOnInit() {
+    this.cartSubTotal = this.cartStore.select(getCartSubTotal);
+    this.cartItemsCount = this.cartStore.select(getCartItemsCount);
+    this.shippingCost = this.cartStore.select(getShippingCost);
+    this.estimatedTax = this.cartStore.select(getEstimatedTax);
+    this.orderTotal = this.cartStore.select(getOrderTotal);
+  }
 }
