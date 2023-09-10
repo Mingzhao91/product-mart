faa3dad9da4e8f25aec4c054784e03b6d929c94b
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-09-10 17:08:42 +0100

    show cart items

--- src/app/cart/shopping-cart/shopping-cart.component.ts
+++ src/app/cart/shopping-cart/shopping-cart.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { CartStore } from '@core/cart/cart-store';
-import { getCartItemsCount } from '@core/cart/cart-selector';
+import { getCartItems, getCartItemsCount } from '@core/cart/cart-selector';
+import { CartItem } from '@core/cart/cart-item';
+import { ALLOWED_PRODUCT_QUANTITIES } from '@core/cart/cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -11,10 +13,23 @@ import { getCartItemsCount } from '@core/cart/cart-selector';
 })
 export class ShoppingCartComponent {
   cartItemsCount!: Observable<number>;
+  cartItems!: Observable<CartItem[]>;
+  availableQuantities!: number[];
+  displayedColumns = ['imgUrl', 'name', 'price', 'quantity', 'remove'];
 
   constructor(private cartStore: CartStore) {}
 
   ngOnInit() {
+    this.availableQuantities = ALLOWED_PRODUCT_QUANTITIES;
     this.cartItemsCount = this.cartStore.select(getCartItemsCount);
+    this.cartItems = this.cartStore.select(getCartItems);
+  }
+
+  updateCartItem($event: { value: number }, cartItem: CartItem) {
+    console.log('Attempting to update quantity from cart page');
+  }
+
+  removeCartItem(cartItem: CartItem) {
+    console.log('Attempting to remove item from cart page');
   }
 }
