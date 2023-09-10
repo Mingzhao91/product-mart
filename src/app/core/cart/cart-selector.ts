3d2b936a82ed368f197df81d5d850d01f539cfe5
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-09-10 12:26:18 +0100

    create getIsItemAlreadyInCart function

--- src/app/core/cart/cart-selector.ts
+++ src/app/core/cart/cart-selector.ts
@@ -1,4 +1,7 @@
+import { createSelector } from 'reselect';
 import { CartState } from './cart-state';
+
+export const getCartItems = (state: CartState) => state.cartItems;
 
 export const getCartItemsCount = (state: CartState) => {
   const cartItems = state.cartItems;
@@ -8,3 +11,9 @@ export const getCartItemsCount = (state: CartState) => {
   );
   return totalCartCount;
 };
+
+export const getIsItemAlreadyInCart = (productId: number) =>
+  createSelector(
+    getCartItems,
+    (items) => items.filter((item) => item.id === productId).length > 0
+  );
--- src/app/core/cart/cart-selectors.spec.ts
+++ src/app/core/cart/cart-selectors.spec.ts
@@ -1,5 +1,5 @@
 import { CartItem } from './cart-item';
-import { getCartItemsCount } from './cart-selector';
+import { getCartItemsCount, getIsItemAlreadyInCart } from './cart-selector';
 import { CartState } from './cart-state';
 
 const given = beforeEach;
@@ -22,7 +22,7 @@ describe('Cart Store Selectors', () => {
       };
 
       const fiveOranges: CartItem = {
-        id: 1,
+        id: 2,
         quantity: 5,
         imgUrl: 'img/orange',
         itemTotal: 20,
@@ -40,5 +40,35 @@ describe('Cart Store Selectors', () => {
     then('I can see my total cart items count', () => {
       expect(result).toBe(15);
     });
+  });
+
+  it('can find cart item', () => {
+    const tenApples: CartItem = {
+      id: 1,
+      quantity: 10,
+      imgUrl: 'img/apple',
+      itemTotal: 20,
+      name: 'apple',
+      price: 2,
+    };
+
+    const fiveOranges: CartItem = {
+      id: 2,
+      quantity: 5,
+      imgUrl: 'img/orange',
+      itemTotal: 20,
+      name: 'orange',
+      price: 2,
+    };
+
+    const state: CartState = {
+      cartItems: [tenApples, fiveOranges],
+    };
+
+    const itemExist = getIsItemAlreadyInCart(2)(state);
+    expect(itemExist).toBeTrue();
+
+    const itemExist1 = getIsItemAlreadyInCart(3)(state);
+    expect(itemExist1).toBeFalse();
   });
 });
