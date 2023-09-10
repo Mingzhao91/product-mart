b76aa238cae50454d51759ad6f9b612c84f65229
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-09-10 16:16:23 +0100

    handle empty cart situation

--- src/app/cart/shopping-cart/shopping-cart.component.ts
+++ src/app/cart/shopping-cart/shopping-cart.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CartStore } from '@core/cart/cart-store';
+import { getCartItemsCount } from '@core/cart/cart-selector';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent {
+  cartItemsCount!: Observable<number>;
 
+  constructor(private cartStore: CartStore) {}
+
+  ngOnInit() {
+    this.cartItemsCount = this.cartStore.select(getCartItemsCount);
+  }
 }
