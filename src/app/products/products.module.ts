a486651dd4bcd377572845058dcdfe7c0a0915c9
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-09-10 12:38:54 +0100

    import shared module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from '@shared/material.module';
+import { SharedModule } from '@shared/shared.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule, MaterialModule],
+  imports: [CommonModule, ProductsRoutingModule, MaterialModule, SharedModule],
 })
 export class ProductsModule {}
